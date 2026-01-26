145cb0a7a93f3e677351bb88776a42a1a4ed9ba5
User <usr@local.pc>
2026-01-26 18:22:57 +0700

    Some binds & opts

--- lua/basics.lua
+++ lua/basics.lua
@@ -2,14 +2,16 @@ vim.cmd("colorscheme gruvbox-material")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.ignorecase = true
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.scrolloff = 15
 vim.opt.sidescrolloff = 3
+vim.opt.wrap = false
+vim.opt.cursorline = true
 
 vim.opt.list = true
 vim.opt.listchars = {
