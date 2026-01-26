909fc880447e359ed848e626d505562cacf14ff3
User <usr@local.pc>
2026-01-26 17:38:14 +0700

    Lualine tweaks

--- lua/noice-conf.lua
+++ lua/noice-conf.lua
@@ -1,7 +1,12 @@
 require("noice").setup({
-    views = {
-        cmdline_popup = {
-            border = { style = "rounded"},
-        }
-    }
+	views = {
+		cmdline_popup = {
+			border = { style = "rounded" },
+		},
+	},
+
+	presets = {
+		long_message_to_split = true,
+	},
 })
+
