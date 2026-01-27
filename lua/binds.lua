59a9e962ac83cb28776f15729ec59ed8c14ab2a4
Nouhelgod <nouhel12@gmail.com>
2026-01-27 22:02:45 +0700

    Finally prettified WhichKey

--- lua/binds.lua
+++ lua/binds.lua
@@ -1,3 +1,6 @@
+-- stylua: ignore start
+-- NOTE: Some keybinds might exceed max line length rule
+
 vim.cmd("command! PIN PlugInstall")
 vim.keymap.set("n", "<space>cc", vim.lsp.buf.hover, {})
 vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
@@ -5,13 +8,7 @@ vim.keymap.set({ "n", "v" }, "<space>ren", vim.lsp.buf.rename, { desc = "Rename 
 vim.keymap.set({ "n", "v" }, "<space>w", ":WhichKey<CR>", { desc = "Which key?" })
 vim.keymap.set({ "n", "v" }, "<space>ff", ":Telescope find_files<CR>", { desc = "Find files" })
 vim.keymap.set({ "n", "v" }, "<space>fg", ":Telescope grep_string<CR>", { desc = "Grep string" })
--- TODO: Tweak this in formatter config
-vim.keymap.set(
-	{ "n", "v" },
-	"<space>fd",
-	':lua require("conform").format({async = true})<CR>',
-	{ desc = "Format code" }
-)
+vim.keymap.set({ "n", "v" },"<space>fd", ':lua require("conform").format({async = true})<CR>', { desc = "Format code" })
 vim.keymap.set({ "n", "v" }, "<space>c/", ":Commentary<CR>", { desc = "Insert comment marker" })
 vim.keymap.set("n", "<C-r>", ":source /home/colada/.config/nvim/init.lua<CR>", { desc = "Reload config" })
 vim.keymap.set("n", "L", ":Neotree action=focus toggle=true<CR>", { desc = "Open filetree" })
--- lua/which-conf.lua
+++ lua/which-conf.lua
@@ -1,8 +1,26 @@
 require("which-key").setup({
-	delay = 50,
+	delay = 200,
+    preset = "helix",
 	plugins = {
 		presets = {
 			motions = true,
+            marks = false,
+            registers = false,
 		},
 	},
+    
+    win = {
+        no_overlap = true,
+        padding = {1, 1},
+        title = true,
+        title_pos = "right",
+        wo = {
+            winblend = 90,
+        },
+        row = 1,
+        border = "none"
+    },
+    layout = {
+        width = { max = 20 },
+    },
 })
